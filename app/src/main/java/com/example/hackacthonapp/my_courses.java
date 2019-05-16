2bb836a0ee333207c99b7c8625db92dbdccdafea
eliko <yyyeliko@gmail.com>
2019-05-16 17:05:31 +0300

    added my courses

--- app/src/main/java/com/example/hackacthonapp/my_courses.java
+++ app/src/main/java/com/example/hackacthonapp/my_courses.java
@@ -9,5 +9,6 @@ public class my_courses extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_courses);
+        System.out.println("started: my courses");
     }
 }
