ae7ade7dc5240259a21cdb5a08db50d97cc93e01
majdi10 <majdi0708@gmail.com>
2019-05-16 18:10:23 +0300

    Add Course screen

---
+++ app/src/main/java/com/example/hackacthonapp/Add_Course.java
@@ -0,0 +1,13 @@
+package com.example.hackacthonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Add_Course extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__course);
+    }
+}
