fb316354d89161a977da64864523f713a85d01fd
eliko <yyyeliko@gmail.com>
2019-05-16 16:41:19 +0300

    added my courses

---
+++ app/src/main/java/com/example/hackacthonapp/my_courses.java
@@ -0,0 +1,13 @@
+package com.example.hackacthonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class my_courses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_courses);
+    }
+}
